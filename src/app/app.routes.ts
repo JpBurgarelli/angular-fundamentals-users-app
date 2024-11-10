7f49486b01a05674317d660d59cdb37b6398e773
joao burgarelli jp <jp.burgarelli@gmail.com>
2024-11-10 19:10:05 -0300

    feat: add users representative page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './pages/users/users.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
   },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+
 ];
