04910de3b9e09f4ff619e8ca44b2899ea015b0a5
joao burgarelli jp <jp.burgarelli@gmail.com>
2024-11-10 18:28:57 -0300

    feat: add fake loader

--- src/app/components/userCard/userCard.component.ts
+++ src/app/components/userCard/userCard.component.ts
@@ -1,8 +1,10 @@
+// userCard.component.ts
 import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { UserService } from '../../services/user.service';
 import { IUser } from '../../models/user.model';
 import { UserProfileModalComponent } from '../userModal/user-profile-modal.component';
@@ -12,26 +14,44 @@ import { SearchFilterComponent } from '../userSearch/user-search.component';
 @Component({
   selector: 'user-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CommonModule, FormsModule, SearchFilterComponent],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    CommonModule,
+    FormsModule,
+    SearchFilterComponent,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './userCard.component.html',
   styleUrls: ['./userCard.component.scss'],
 })
 export class UserCard implements OnInit {
   users: IUser[] = [];
   filteredUsers: IUser[] = [];
+  isLoading = false;
 
   constructor(private userService: UserService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe(
-      (data: IUser[]) => {
-        this.users = data;
-        this.filteredUsers = data;
-      },
-      (error) => {
-        console.log('Erro na busca pelos usuários!', error);
-      }
-    );
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.isLoading = true;
+
+    setTimeout(() => {
+      this.userService.getUsers().subscribe({
+        next: (data: IUser[]) => {
+          this.users = data;
+          this.filteredUsers = data;
+          this.isLoading = false;
+        },
+        error: (error) => {
+          console.log('Erro na busca pelos usuários!', error);
+          this.isLoading = false;
+        }
+      });
+    }, 1500);
   }
 
   onFilterChange(searchQuery: string): void {
@@ -47,4 +67,3 @@ export class UserCard implements OnInit {
     });
   }
 }
-
